13146f5b24d630edda0c6d6fb21002370fc1ad1c
Carlos Gonçalves <carlosmoliveiragoncalves@gmail.com>
2021-07-15 14:35:26 +0100

    Added some JSONB serializer to test potential bug

--- webservices/src/main/java/pt/test/webservices/UserResource.java
+++ webservices/src/main/java/pt/test/webservices/UserResource.java
@@ -3,10 +3,13 @@ package pt.test.webservices;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import fish.payara.micro.cdi.Outbound;
+import pt.test.jsonb.ClientApp;
+import pt.test.jsonb.JsonbConverter;
 
 @Stateless
 @Path("/users")
@@ -16,6 +19,9 @@ public class UserResource {
     @Outbound
     private Event<String> event;
 
+    @Inject
+    private JsonbConverter jsonbConverter;
+
     @POST
     @Path("sendevent")
     public Response runUpdateExchangeRate() {
@@ -23,4 +29,11 @@ public class UserResource {
         return Response.ok().build();
     }
 
+    @GET
+    public Response testPair() {
+
+        ClientApp json = jsonbConverter.convertFromJson("{\"appName\":\"Payara\",\"appVersion\":\"5\"}", ClientApp.class);
+
+        return Response.ok(json).build();
+    }
 }
